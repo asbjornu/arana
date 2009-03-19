975f5d6271341010c0cba7184d539c3c20dda340
asbjornu <asbjornu@9307df46-1491-11de-99d6-5fb3f1362b5f>
2009-03-19 14:58:35 +0000

    Initial check-in

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using Arana.Core;
+
+namespace Arana.Test.ConsoleApplication
+{
+   internal class Program
+   {
+      private static void Main()
+      {
+         try
+         {
+            TestArana();
+         }
+         catch (Exception ex)
+         {
+            Console.WriteLine(ex);
+         }
+
+         Console.WriteLine();
+         Console.WriteLine();
+         Console.WriteLine("Press any key to exit.");
+         Console.ReadKey(true);
+      }
+
+
+      private static void TestArana()
+      {
+         AranaEngine engine = new AranaEngine("http://localhost/Ruter.MinRuter/trunk/");
+         engine.Select("div.login a").Follow();
+         Console.WriteLine(engine.Select("title").InnerText);
+      }
+   }
+}
---
+++ Core/Extensions/StringExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Arana.Core.Extensions
+{
+   public static class StringExtensions
+   {
+      public static bool StartsWith(this string @s, params string[] values)
+      {
+         if (String.IsNullOrEmpty(s) || values == null || values.Length == 0)
+            return false;
+
+         foreach (string value in values)
+         {
+            if (s.StartsWith(value))
+               return true;
+         }
+
+         return false;
+      }
+   }
+}
