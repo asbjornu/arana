1a5b13f6990fd3a1e2fe294545c8b088366dc03b
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2009-03-19 14:58:35 +0000

    Initial check-in

---
+++ Core/AranaEngine.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+
+using Fizzler.Parser;
+
+using HtmlAgilityPack;
+
+using Arana.Core.Extensions;
+
+namespace Arana.Core
+{
+   /// <summary>
+   /// The base testing engine of Araña.
+   /// </summary>
+   public class AranaEngine
+   {
+      private Uri baseUri;
+      private SelectorEngine engine;
+
+
+      /// <summary>
+      /// Initializes a new instance of the <see cref="Arana"/> class.
+      /// </summary>
+      /// <param name="uri">The application URI.</param>
+      public AranaEngine(string uri)
+      {
+         GoTo(uri);
+      }
+
+
+      /// <summary>
+      /// Selects the specified xpath expression.
+      /// </summary>
+      /// <param name="cssSelector">The CSS selector.</param>
+      /// <returns></returns>
+      public ElementList Select(string cssSelector)
+      {
+         IList<HtmlNode> nodes = this.engine.Parse(cssSelector);
+
+         if (nodes == null || nodes.Count == 0)
+            throw new ArgumentException("The CSS selector returned an empty node set.",
+                                        "cssSelector");
+
+         return new ElementList(nodes, this);
+      }
+
+
+      /// <summary>
+      /// Goes to the URI.
+      /// </summary>
+      /// <param name="uri">The URI.</param>
+      internal void GoTo(string uri)
+      {
+         this.engine = GetSelectorEngine(uri);
+      }
+
+      // internal void GoTo(string uri)
+
+
+      /// <summary>
+      /// Gets the document.
+      /// </summary>
+      /// <param name="uri">The URI.</param>
+      /// <returns></returns>
+      private SelectorEngine GetSelectorEngine(string uri)
+      {
+         Uri u = GetUri(uri);
+         baseUri = new Uri(u.GetLeftPart(UriPartial.Authority));
+
+         HttpWebRequest request = HttpWebRequest.Create(u) as HttpWebRequest;
+
+         if (request == null)
+            throw new ArgumentException("The URI did not make much sense, sorry.", "uri");
+
+         HttpWebResponse response = request.GetHttpWebResponse();
+
+         if (response == null)
+            throw new ArgumentException("The URI did not make much sense, sorry.", "uri");
+
+         using (StreamReader streamReader = new StreamReader(response.GetResponseStream()))
+         {
+            string html = streamReader.ReadToEnd();
+            return new SelectorEngine(html);
+         }
+      }
+
+
+      /// <summary>
+      /// Gets the URI.
+      /// </summary>
+      /// <param name="uri">The URI.</param>
+      /// <returns></returns>
+      private Uri GetUri(string uri)
+      {
+         try
+         {
+            if (uri.StartsWith("http://") || uri.StartsWith("https://"))
+               return new Uri(uri);
+
+            return new Uri(this.baseUri, uri);
+         }
+         catch (UriFormatException ex)
+         {
+            throw new InvalidOperationException(
+               String.Format("Can't create an URI from '{0}'.", uri), ex);
+         }
+      }
+   }
+}
---
+++ Core/ElementList.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using HtmlAgilityPack;
+
+namespace Arana.Core
+{
+   /// <summary>
+   /// Provides an encapsulation of <see cref="HtmlNode" />.
+   /// </summary>
+   public class ElementList : List<HtmlNode>
+   {
+      private readonly AranaEngine exerciser;
+
+
+      /// <summary>
+      /// Initializes a new instance of the <see cref="ElementList"/> class.
+      /// </summary>
+      /// <param name="htmlNodes">The HTML nodes.</param>
+      /// <param name="exerciser">The exerciser.</param>
+      internal ElementList(IEnumerable<HtmlNode> htmlNodes, AranaEngine exerciser)
+         : base(htmlNodes)
+      {
+         this.exerciser = exerciser;
+      }
+
+
+      /// <summary>
+      /// Gets the <see cref="HtmlAgilityPack.HtmlNode"/> with the specified tag name.
+      /// </summary>
+      /// <value></value>
+      public HtmlNode this[string tagName]
+      {
+         get
+         {
+            foreach (HtmlNode htmlNode in this)
+               if (String.Compare(htmlNode.Name, tagName, true) == 0)
+                  return htmlNode;
+
+            return null;
+         }
+      }
+
+
+      /// <summary>
+      /// Gets the inner HTML.
+      /// </summary>
+      /// <value>The inner HTML.</value>
+      public string InnerHtml
+      {
+         get
+         {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (HtmlNode htmlNode in this)
+               sb.AppendLine(htmlNode.InnerHtml);
+
+            return sb.ToString().Trim();
+         }
+      }
+
+      /// <summary>
+      /// Gets the inner text.
+      /// </summary>
+      /// <value>The inner text.</value>
+      public string InnerText
+      {
+         get
+         {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (HtmlNode htmlNode in this)
+               sb.AppendLine(htmlNode.InnerText);
+
+            return sb.ToString().Trim();
+         }
+      }
+
+
+      /// <summary>
+      /// Follows the 'href' attribute on the selected HTML elements.
+      /// </summary>
+      /// <returns></returns>
+      public AranaEngine Follow()
+      {
+         HtmlNode anchor = this["a"];
+
+         if (anchor == null)
+            throw new InvalidOperationException("The selected elements does not contain an HTML 'a' element.");
+
+         if ((anchor.Attributes == null) || (anchor.Attributes.Count == 0))
+            throw new InvalidOperationException("The HTML anchor has no attributes.");
+
+         HtmlAttribute hrefAttribute = anchor.Attributes["href"];
+
+         if ((hrefAttribute == null) || String.IsNullOrEmpty(hrefAttribute.Value))
+            throw new InvalidOperationException("The HTML anchor has an empty 'href' attribute.");
+
+         this.exerciser.GoTo(hrefAttribute.Value);
+         return this.exerciser;
+      }
+
+
+      /// <summary>
+      /// Submits the selected 'form' element, given its 'action' attribute.
+      /// </summary>
+      /// <returns></returns>
+      public AranaEngine Submit()
+      {
+         HtmlNode form = this["form"];
+
+         if (form == null)
+            throw new InvalidOperationException("The selected elements does not contain an HTML 'form' element.");
+
+         if ((form.Attributes == null) || (form.Attributes.Count == 0))
+            throw new InvalidOperationException("The HTML form has no attributes.");
+
+         HtmlAttribute actionAttribute = form.Attributes["action"];
+
+         if ((actionAttribute == null) || String.IsNullOrEmpty(actionAttribute.Value))
+            throw new InvalidOperationException("The HTML form has an empty 'action' attribute.");
+
+         // TODO: Add submit action
+
+         return this.exerciser;
+      }
+   }
+}
