e94baee3520bbc3b1acaf3e252141f66e6945284
asbjornu <asbjornu@9307df46-1491-11de-99d6-5fb3f1362b5f>
2009-03-25 10:51:21 +0000

    Move values to config file.

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.Collections.Specialized;
 
 using Arana.Core;
 
 namespace Arana.Test.ConsoleApplication
 {
-   internal class Program
+   internal static class Program
    {
       private static void Main()
       {
@@ -17,18 +18,39 @@ namespace Arana.Test.ConsoleApplication
             Console.WriteLine(ex);
          }
 
-         Console.WriteLine();
-         Console.WriteLine();
-         Console.WriteLine("Press any key to exit.");
-         Console.ReadKey(true);
+         PressAnyKey("Press any key to exit.");
       }
 
 
       private static void TestArana()
       {
-         AranaEngine engine = new AranaEngine("http://example.com/");
+         AranaEngine engine = new AranaEngine(Settings.WebSiteUri);
+         
          engine.Select("div.login a").Follow();
          Console.WriteLine(engine.Select("title").InnerText);
+
+         engine.Select("form#aspnetForm").Submit(false, new NameValueCollection
+         {
+            {"email", Settings.Email},
+            {"password", Settings.Password},
+         });
+
+         Console.WriteLine(engine.Response.Location);
+
+         /*Console.WriteLine(engine.Select("title").InnerText);
+
+         engine.Select(".card-overview .section.settings a").Follow();
+
+         Console.WriteLine(engine.Select("title").InnerText);*/
+      }
+
+      private static void PressAnyKey(string message)
+      {
+         Console.WriteLine();
+         Console.WriteLine();
+         Console.WriteLine(message);
+         Console.WriteLine();
+         Console.ReadKey(true);
       }
    }
 }
