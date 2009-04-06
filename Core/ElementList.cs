24501f3d6b4a1e1e0bbe40020924084c659cf54a
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2009-04-06 14:22:04 +0000

    Not tested well enough, but hopefully this major refactoring and rewriting should work. Not backwards compatible in any way.

--- Core/ElementList.cs
+++ Core/ElementList.cs
@@ -1,18 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.Linq;
 using System.Text;
 
 using Arana.Core.Extensions;
 
 using HtmlAgilityPack;
 
+// ReSharper disable UnusedMember.Global
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable UnusedMethodReturnValue.Global
+
 namespace Arana.Core
 {
    /// <summary>
-   /// Provides an encapsulation of <see cref="List&lt;HtmlNode&gt;" />.
+   /// Provides an encapsulation of <see cref="List{HtmlNode}" />.
    /// </summary>
-   public class ElementList : List<HtmlNode>
+   public partial class ElementList : List<HtmlNode>
    {
       /// <summary>
       /// Contains the tag names of all HTML form elements that should be posted to
@@ -29,6 +34,9 @@ namespace Arana.Core
       /// </summary>
       private static readonly string FormElementsSelector = String.Join(",", FormElements);
 
+      /// <summary>
+      /// The <see cref="AranaEngine" /> to use for requests.
+      /// </summary>
       private readonly AranaEngine engine;
 
 
@@ -36,29 +44,35 @@ namespace Arana.Core
       /// Initializes a new instance of the <see cref="ElementList"/> class.
       /// </summary>
       /// <param name="htmlNodes">The HTML nodes.</param>
-      /// <param name="exerciser">The engine.</param>
-      internal ElementList(IEnumerable<HtmlNode> htmlNodes, AranaEngine exerciser)
+      /// <param name="engine">The engine.</param>
+      internal ElementList(IEnumerable<HtmlNode> htmlNodes, AranaEngine engine)
          : base(htmlNodes)
       {
-         this.engine = exerciser;
+         this.engine = engine;
       }
 
 
       /// <summary>
-      /// Gets the <see cref="HtmlAgilityPack.HtmlNode"/> with the specified tag name.
+      /// Gets the <see cref="ElementList"/> with the specified <param name="tagName"/>.
       /// </summary>
-      /// <value></value>
-      // ReSharper disable MemberCanBePrivate.Global
-      public HtmlNode this[string tagName]
-         // ReSharper restore MemberCanBePrivate.Global
+      /// <value>
+      /// The <see cref="ElementList"/> with the specified <param name="tagName"/>.
+      /// </value>
+      /// <exception cref="ArgumentNullException">
+      /// If <paramref name="tagName"/> is null or empty.
+      /// </exception>
+      public ElementList this[string tagName]
       {
          get
          {
-            foreach (HtmlNode htmlNode in this)
-               if (String.Compare(htmlNode.Name, tagName, true) == 0)
-                  return htmlNode;
+            if (String.IsNullOrEmpty(tagName))
+               throw new ArgumentNullException("tagName");
 
-            return null;
+            var v = from htmlNode in this
+                    where (String.Compare(htmlNode.Name, tagName, true) == 0)
+                    select htmlNode;
+
+            return new ElementList(new List<HtmlNode>(v), this.engine);
          }
       }
 
@@ -67,9 +81,7 @@ namespace Arana.Core
       /// Gets the inner HTML of the currently selected list of elements.
       /// </summary>
       /// <value>The inner HTML of the currently selected list of elements.</value>
-      // ReSharper disable UnusedMember.Global
       public string InnerHtml
-         // ReSharper restore UnusedMember.Global
       {
          get
          {
@@ -99,44 +111,6 @@ namespace Arana.Core
          }
       }
 
-      /// <summary>
-      /// Gets the value of the 'value' attribute of the currently selected list of elements.
-      /// </summary>
-      /// <value>The value of the 'value' attribute of the currently selected list of elements.</value>
-      public string Value
-      {
-         get { return Attribute("value"); }
-      }
-
-
-      /// <summary>
-      /// Gets the value of the attribute with the given <paramref name="name"/>
-      /// of the currently selected list of elements.
-      /// </summary>
-      /// <param name="name">The name of the attribute whose value to retrieve.</param>
-      /// <returns>
-      /// The value of the attribute with the given <paramref name="name"/> of
-      /// the currently selected list of elements.
-      /// </returns>
-      // ReSharper disable MemberCanBePrivate.Global
-      public string Attribute(string name)
-         // ReSharper restore MemberCanBePrivate.Global
-      {
-         StringBuilder sb = new StringBuilder();
-
-         foreach (HtmlNode htmlNode in this)
-         {
-            HtmlAttribute attribute = htmlNode.Attributes[name];
-
-            if (attribute == null)
-               continue;
-
-            sb.AppendLine(attribute.Value);
-         }
-
-         return sb.ToString().Trim();
-      }
-
 
       /// <summary>
       /// Follows the 'href' attribute on the selected HTML elements.
@@ -160,29 +134,68 @@ namespace Arana.Core
       /// Internet resource; otherwise, <c>false</c>. The default value is true.</param>
       /// <returns>An updated <see cref="AranaEngine"/>.</returns>
       /// <exception cref="InvalidOperationException">
-      /// 1. If the currently selected elements doesn't contain an 'a' element.
-      /// 2. If a currently selected 'a' element doesn't have any attributes.
-      /// 3. If a currently selected 'a' element has an empty or non-existent 'href' attribute.
+      /// 	<list type="number">
+      /// 		<item>If the currently selected elements doesn't contain an 'a' element.</item>
+      /// 		<item>If a currently selected 'a' element doesn't have any attributes.</item>
+      /// 		<item>If a currently selected 'a' element has an empty or non-existent 'href' attribute.</item>
+      /// 	</list>
       /// </exception>
-      // ReSharper disable MemberCanBePrivate.Global
       public AranaEngine Follow(bool followRedirect)
-         // ReSharper restore MemberCanBePrivate.Global
       {
-         HtmlNode anchor = this["a"];
+         ElementList anchors = this["a"];
 
-         if (anchor == null)
-            throw new InvalidOperationException("The selected elements does not contain an HTML 'a' element.");
+         if (anchors.Count == 0)
+            throw new InvalidOperationException("The selected element(s) does not contain an HTML 'a' element.");
 
-         if ((anchor.Attributes == null) || (anchor.Attributes.Count == 0))
+         if ((anchors.Attributes() == null) || (anchors.Attributes().Count() == 0))
             throw new InvalidOperationException("The HTML anchor has no attributes.");
 
-         HtmlAttribute hrefAttribute = anchor.Attributes["href"];
+         string href = anchors.Attribute("href");
 
-         if ((hrefAttribute == null) || String.IsNullOrEmpty(hrefAttribute.Value))
+         if (String.IsNullOrEmpty(href))
             throw new InvalidOperationException("The HTML anchor has an empty 'href' attribute.");
 
-         this.engine.NavigateTo(hrefAttribute.Value, followRedirect);
+         this.engine.NavigateTo(href, followRedirect);
+
          return this.engine;
+      }
+
+
+      /// <summary>
+      /// Returns the very next sibling for each <see cref="T:HtmlAgilityPack.HtmlNode"/>
+      /// in the list of currently selected elements.
+      /// </summary>
+      /// <returns>
+      /// The very next sibling for each <see cref="T:HtmlAgilityPack.HtmlNode"/>
+      /// in the list of currently selected elements.
+      /// </returns>
+      public ElementList Next()
+      {
+         var v = from htmlNode in this
+                 let nextSibling = htmlNode.NextSibling
+                 where (nextSibling != null)
+                 select nextSibling;
+
+         return new ElementList(new List<HtmlNode>(v), this.engine);
+      }
+
+
+      /// <summary>
+      /// Returns the very previous sibling for each <see cref="T:HtmlAgilityPack.HtmlNode"/>
+      /// in the list of currently selected elements.
+      /// </summary>
+      /// <returns>
+      /// The very previous sibling for each <see cref="T:HtmlAgilityPack.HtmlNode"/>
+      /// in the list of currently selected elements.
+      /// </returns>
+      public ElementList Previous()
+      {
+         var v = from htmlNode in this
+                 let nextSibling = htmlNode.PreviousSibling
+                 where (nextSibling != null)
+                 select nextSibling;
+
+         return new ElementList(new List<HtmlNode>(v), this.engine);
       }
 
 
@@ -195,9 +208,7 @@ namespace Arana.Core
       /// 2. If a currently selected 'form' element doesn't have any attributes.
       /// 3. If a currently selected 'form' element has an empty or non-existent 'action' attribute.
       /// </exception>
-      // ReSharper disable UnusedMember.Global
       public AranaEngine Submit()
-         // ReSharper restore UnusedMember.Global
       {
          return Submit(true, null);
       }
@@ -213,9 +224,7 @@ namespace Arana.Core
       /// 2. If a currently selected 'form' element doesn't have any attributes.
       /// 3. If a currently selected 'form' element has an empty or non-existent 'action' attribute.
       /// </exception>
-      // ReSharper disable UnusedMethodReturnValue.Global
       public AranaEngine Submit(NameValueCollection requestValues)
-         // ReSharper restore UnusedMethodReturnValue.Global
       {
          return Submit(true, requestValues);
       }
@@ -229,40 +238,72 @@ namespace Arana.Core
       /// <param name="requestValues">The request values.</param>
       /// <returns>An updated <see cref="AranaEngine"/>.</returns>
       /// <exception cref="InvalidOperationException">
-      /// 1. If the currently selected elements doesn't contain a 'form' element.
-      /// 2. If a currently selected 'form' element doesn't have any attributes.
-      /// 3. If a currently selected 'form' element has an empty or non-existent 'action' attribute.
+      /// 	<list type="number">
+      /// 		<item>If the currently selected elements doesn't contain a 'form' element.</item>
+      /// 		<item>If a currently selected 'form' element has an empty or non-existent 'action' attribute and an action URI can't be deduced from a previous <see cref="AranaRequest"/>.</item>
+      /// 	</list>
       /// </exception>
       public AranaEngine Submit(bool followRedirect, NameValueCollection requestValues)
       {
-         HtmlNode form = this["form"];
+         ElementList form = this["form"];
 
-         if (form == null)
-            throw new InvalidOperationException("The selected elements does not contain an HTML 'form' element.");
+         if (form.Count == 0)
+            throw new InvalidOperationException(
+               "The selected element(s) does not contain an HTML 'form' element.");
 
-         HtmlAttribute actionAttribute = form.Attributes["action"];
-         HtmlAttribute methodAttribute = form.Attributes["method"];
+         string method = form.Attribute("method");
+         string action = form.Attribute("action");
          RequestDictionary requestDictionary = null;
 
-         string method = (methodAttribute != null) && !String.IsNullOrEmpty(methodAttribute.Value)
-                            ? methodAttribute.Value.ToUpperInvariant()
-                            : HttpMethod.Get;
+         // Set the HTTP method. Default to "GET".
+         method = String.IsNullOrEmpty(method)
+                     ? HttpMethod.Get
+                     : method.ToUpperInvariant();
 
-         if ((form.Attributes == null) || (form.Attributes.Count == 0))
-            throw new InvalidOperationException("The HTML form has no attributes.");
+         // Set the URI to post the form to. Default to the current URI.
+         action = String.IsNullOrEmpty(action)
+                     ? this.engine.Uri.ToString()
+                     : action;
 
-         if ((actionAttribute == null) || String.IsNullOrEmpty(actionAttribute.Value))
-            throw new InvalidOperationException("The HTML form has an empty 'action' attribute.");
+         if (String.IsNullOrEmpty(action))
+            throw new InvalidOperationException("No valid 'action' to perform.");
 
-         // TODO: Make the CSS selector more precise, so it only select child nodes of the current form element.
-         ElementList formElements = this.engine.Select(FormElementsSelector);
+         ElementList formElements = this.engine.Select(FormElementsSelector, form);
 
          if ((formElements != null) && (formElements.Count > 0))
             requestDictionary = formElements.GetRequestCollection(requestValues);
 
-         this.engine.NavigateTo(actionAttribute.Value, followRedirect, method, requestDictionary);
+         this.engine.NavigateTo(action, followRedirect, method, requestDictionary);
 
          return this.engine;
+      }
+
+
+      /// <summary>
+      /// Determines whether any of the elements in the currently selected list
+      /// are parents of the specified <paramref name="node"/>.
+      /// </summary>
+      /// <param name="node">The HTML node.</param>
+      /// <returns>
+      /// 	<c>true</c> if any of the elements in the currently selected list are
+      /// parents of the specified node; otherwise, <c>false</c>.
+      /// </returns>
+      /// <exception cref="ArgumentNullException">
+      /// If <paramref name="node"/> is null.
+      /// </exception>
+      internal bool IsParentOf(HtmlNode node)
+      {
+         if (node == null)
+            throw new ArgumentNullException("node");
+
+         HtmlNode parentNode;
+
+         while ((parentNode = node.ParentNode) != null)
+            foreach (HtmlNode htmlNode in this)
+               if (parentNode == htmlNode)
+                  return true;
+
+         return false;
       }
 
 
@@ -293,14 +334,14 @@ namespace Arana.Core
             HtmlAttribute valueAttribute = node.Attributes["value"];
 
             // Skip elements that donesn't have a valid 'name' attribute
-            if ((nameAttribute == null) || String.IsNullOrEmpty(nameAttribute.Value))
+            if (!nameAttribute.HasValue())
                continue;
 
             string name = nameAttribute.Value;
 
             // Retrieve the value for the given form element from the request values collection.
             string value = requestValues[name];
-            value = String.IsNullOrEmpty(value) && (valueAttribute != null)
+            value = String.IsNullOrEmpty(value) && valueAttribute.HasValue()
                        ? valueAttribute.Value
                        : value;
 
---
+++ Core/Extensions/HtmlAttributeExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using HtmlAgilityPack;
+
+namespace Arana.Core.Extensions
+{
+   /// <summary>
+   /// Contains extension methods for the <see cref="HtmlAttribute" /> object.
+   /// </summary>
+   internal static class HtmlAttributeExtensions
+   {
+      /// <summary>
+      /// Determines whether the specified attribute has value.
+      /// </summary>
+      /// <param name="attribute">The attribute.</param>
+      /// <returns>
+      /// 	<c>true</c> if the specified attribute has value; otherwise, <c>false</c>.
+      /// </returns>
+      public static bool HasValue(this HtmlAttribute @attribute)
+      {
+         return (attribute != null) && !String.IsNullOrEmpty(attribute.Value);
+      }
+   }
+}
