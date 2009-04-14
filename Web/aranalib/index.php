f629bd6e831634d13c5bf3e94758a35999542e08
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2009-04-14 14:15:38 +0000

    Web site and test harness setup and initial check-in.

---
+++ Web/aranalib/index.php
@@ -0,0 +1,16 @@
+<?php header('Content-Type: text/html; charset=utf-8'); ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+   <head>
+       <title>Araña Library</title>
+       <meta http-equiv="Content-Type" content="application/xhtml+xml; charset=utf-8" />
+   </head>
+   <body>
+      <h1>Araña Library</h1>
+      <p>
+        <strong>Araña</strong> ("spider" in Spanish) is a simple web testing library, written in C#.
+        It can be used to integrate simple testing of web applications into unit testing, so the parts
+        of your web application can be tested separately.
+      </p>
+   </body>
+</html>
