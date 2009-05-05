285cb6c7723c3e222eafedd5e409a5251585f09f
asbjornu <asbjornu@9307df46-1491-11de-99d6-5fb3f1362b5f>
2009-05-05 12:46:22 +0000

    Some refactoring and version increment.

--- Core/Fizzler.Systems.HtmlAgilityPack/HtmlNodeExtensions.cs
+++ Core/Fizzler.Systems.HtmlAgilityPack/HtmlNodeExtensions.cs
@@ -14,64 +14,89 @@ namespace Arana.Core.Fizzler.Systems.HtmlAgilityPack
       /// <summary>
       /// Determines whether this node is an element or not.
       /// </summary>
-      private static bool IsElement(this HtmlNode node)
+      /// <param name="node">The node.</param>
+      /// <returns>
+      /// 	<c>true</c> if the specified node is element; otherwise, <c>false</c>.
+      /// </returns>
+      private static bool IsElement(this HtmlNode @node)
       {
          if (node == null)
             throw new ArgumentNullException("node");
+
          return node.NodeType == HtmlNodeType.Element;
       }
 
       /// <summary>
       /// Returns a collection of elements from this collection.
       /// </summary>
-      public static IEnumerable<HtmlNode> Elements(this IEnumerable<HtmlNode> nodes)
+      /// <param name="nodes">The nodes.</param>
+      /// <returns></returns>
+      public static IEnumerable<HtmlNode> Elements(this IEnumerable<HtmlNode> @nodes)
       {
          if (nodes == null)
             throw new ArgumentNullException("nodes");
+
          return nodes.Where(n => n.IsElement());
       }
 
       /// <summary>
       /// Returns a collection of child nodes of this node.
       /// </summary>
-      private static IEnumerable<HtmlNode> Children(this HtmlNode node)
+      /// <param name="node">The node.</param>
+      /// <returns></returns>
+      private static IEnumerable<HtmlNode> Children(this HtmlNode @node)
       {
          if (node == null)
             throw new ArgumentNullException("node");
+
          return node.ChildNodes.Cast<HtmlNode>();
       }
 
       /// <summary>
       /// Returns a collection of child elements of this node.
       /// </summary>
-      public static IEnumerable<HtmlNode> Elements(this HtmlNode node)
+      /// <param name="node">The node.</param>
+      /// <returns></returns>
+      public static IEnumerable<HtmlNode> Elements(this HtmlNode @node)
       {
          if (node == null)
             throw new ArgumentNullException("node");
+
          return node.Children().Elements();
       }
 
       /// <summary>
       /// Returns a collection of the sibling elements after this node.
       /// </summary>
-      public static IEnumerable<HtmlNode> ElementsAfterSelf(this HtmlNode node)
+      /// <param name="node">The node.</param>
+      /// <returns></returns>
+      public static IEnumerable<HtmlNode> ElementsAfterSelf(this HtmlNode @node)
       {
          if (node == null)
             throw new ArgumentNullException("node");
+
          return node.NodesAfterSelf().Elements();
       }
 
       /// <summary>
       /// Returns a collection of the sibling nodes after this node.
       /// </summary>
-      private static IEnumerable<HtmlNode> NodesAfterSelf(this HtmlNode node)
+      /// <param name="node">The node.</param>
+      /// <returns></returns>
+      private static IEnumerable<HtmlNode> NodesAfterSelf(this HtmlNode @node)
       {
          if (node == null)
             throw new ArgumentNullException("node");
+
          return NodesAfterSelfImpl(node);
       }
 
-      private static IEnumerable<HtmlNode> NodesAfterSelfImpl(HtmlNode node)
+      /// <summary>
+      /// Nodeses the after self impl.
+      /// </summary>
+      /// <param name="node">The node.</param>
+      /// <returns></returns>
+      private static IEnumerable<HtmlNode> NodesAfterSelfImpl(HtmlNode @node)
       {
          while ((node = node.NextSibling) != null)
             yield return node;
@@ -80,24 +105,30 @@ namespace Arana.Core.Fizzler.Systems.HtmlAgilityPack
       /// <summary>
       /// Returns a collection of the sibling elements before this node.
       /// </summary>
-      public static IEnumerable<HtmlNode> ElementsBeforeSelf(this HtmlNode node)
+      /// <param name="node">The node.</param>
+      /// <returns></returns>
+      public static IEnumerable<HtmlNode> ElementsBeforeSelf(this HtmlNode @node)
       {
          if (node == null)
             throw new ArgumentNullException("node");
+
          return node.NodesBeforeSelf().Elements();
       }
 
       /// <summary>
       /// Returns a collection of the sibling nodes before this node.
       /// </summary>
-      private static IEnumerable<HtmlNode> NodesBeforeSelf(this HtmlNode node)
+      /// <param name="node">The node.</param>
+      /// <returns></returns>
+      private static IEnumerable<HtmlNode> NodesBeforeSelf(this HtmlNode @node)
       {
          if (node == null)
             throw new ArgumentNullException("node");
+
          return NodesBeforeSelfImpl(node);
       }
 
-      private static IEnumerable<HtmlNode> NodesBeforeSelfImpl(HtmlNode node)
+      private static IEnumerable<HtmlNode> NodesBeforeSelfImpl(HtmlNode @node)
       {
          while ((node = node.PreviousSibling) != null)
             yield return node;
@@ -106,16 +137,20 @@ namespace Arana.Core.Fizzler.Systems.HtmlAgilityPack
       /// <summary>
       /// Returns a collection of all descendant nodes of this element.
       /// </summary>
-      public static IEnumerable<HtmlNode> Descendants(this HtmlNode node)
+      /// <param name="node">The node.</param>
+      /// <returns></returns>
+      public static IEnumerable<HtmlNode> Descendants(this HtmlNode @node)
       {
          if (node == null)
             throw new ArgumentNullException("node");
+
          return DescendantsImpl(node);
       }
 
       private static IEnumerable<HtmlNode> DescendantsImpl(HtmlNode node)
       {
          Debug.Assert(node != null);
+
          foreach (HtmlNode child in node.ChildNodes)
          {
             yield return child;
