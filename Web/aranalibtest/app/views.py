aaf556617ae672890b685199795de2273f2642aa
asbjornu <asbjornu@9307df46-1491-11de-99d6-5fb3f1362b5f>
2009-05-05 14:17:32 +0000

    First test implementation

--- Web/aranalibtest/app/views.py
+++ Web/aranalibtest/app/views.py
@@ -5,6 +5,22 @@ from django.template import Context, loader
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 
-def home(request):
-  template = loader.get_template('main.html')
+def index(request):
+  template = loader.get_template('index.html')
   return HttpResponse(template.render(Context()))
+
+def simple_post_test(request):
+  # initialize request values
+  context = Context({
+    'textbox'   : request.POST.get('textbox',   ''),
+    'radio'     : request.POST.get('radio',     ''),
+    'textarea'  : request.POST.get('textarea',  ''),
+    'select'    : request.POST.get('select',    ''),
+    'submit'    : request.POST.get('submit',    ''),
+  })
+  
+  # set up the template
+  template = loader.get_template('simple_post_test.html')
+  
+  # render the thing
+  return HttpResponse(template.render(context))
