85a8b119633770fabb0f69dbc5bd37af3364d950
asbjornu <asbjornu@9307df46-1491-11de-99d6-5fb3f1362b5f>
2009-05-05 10:55:51 +0000

    Test changes and initial test harness check-in

---
+++ Core/Selection/Selection.ListImplementation.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using HtmlAgilityPack;
+
+namespace Arana.Core
+{
+   public partial class Selection : IList<HtmlNode>
+   {
+      private readonly List<HtmlNode> nodes;
+
+      #region IList<HtmlNode> Members
+
+      public int IndexOf(HtmlNode item)
+      {
+         return this.nodes.IndexOf(item);
+      }
+
+      public void Insert(int index, HtmlNode item)
+      {
+         this.nodes.Insert(index, item);
+      }
+
+      public void RemoveAt(int index)
+      {
+         this.nodes.RemoveAt(index);
+      }
+
+      public HtmlNode this[int index]
+      {
+         get { return this.nodes[index]; }
+         set { this.nodes[index] = value; }
+      }
+
+      public void Add(HtmlNode item)
+      {
+         this.nodes.Add(item);
+      }
+
+      public void Clear()
+      {
+         this.nodes.Clear();
+      }
+
+      public bool Contains(HtmlNode item)
+      {
+         return this.nodes.Contains(item);
+      }
+
+      public void CopyTo(HtmlNode[] array, int arrayIndex)
+      {
+         this.nodes.CopyTo(array, arrayIndex);
+      }
+
+      public bool IsReadOnly
+      {
+         get { return false; }
+      }
+
+      public bool Remove(HtmlNode item)
+      {
+         return this.nodes.Remove(item);
+      }
+
+      public IEnumerator<HtmlNode> GetEnumerator()
+      {
+         return this.nodes.GetEnumerator();
+      }
+
+      IEnumerator IEnumerable.GetEnumerator()
+      {
+         return this.nodes.GetEnumerator();
+      }
+
+      public int Count
+      {
+         get { return this.nodes.Count; }
+      }
+
+      #endregion
+   }
+}
--- Core/Selection/Selection.cs
+++ Core/Selection/Selection.cs
@@ -15,7 +15,7 @@ namespace Arana.Core
    /// <summary>
    /// A list of HTML elements selected with <see cref="AranaEngine.Select(System.String)" />.
    /// </summary>
-   public partial class Selection : List<HtmlNode>
+   public partial class Selection
    {
       /// <summary>
       /// Contains the tag names of all HTML form elements that should be posted to
@@ -64,8 +64,8 @@ namespace Arana.Core
       /// </summary>
       /// <param name="nodes">The nodes.</param>
       private Selection(IEnumerable<HtmlNode> nodes)
-         : base(nodes)
       {
+         this.nodes = new List<HtmlNode>(nodes);
       }
 
 
@@ -141,7 +141,6 @@ namespace Arana.Core
       /// </summary>
       /// <value>The CSS selector.</value>
       internal string CssSelector { get; private set; }
-
 
       /// <summary>
       /// Returns a <see cref="T:System.String"/> that represents the current <see cref="T:System.Object"/>.
@@ -250,136 +249,6 @@ namespace Arana.Core
                                    select nextSibling;
 
          return new Selection(v, this.engine);
-      }
-
-
-      /// <summary>
-      /// Submits the selected 'form' element, given its 'action' attribute.
-      /// </summary>
-      /// <param name="formElementsSelection">The form elements selection.</param>
-      /// <returns>An updated <see cref="AranaEngine"/>.</returns>
-      /// <exception cref="InvalidOperationException">
-      /// 1. If the currently selected elements doesn't contain a 'form' element.
-      /// 2. If a currently selected 'form' element doesn't have any attributes.
-      /// 3. If a currently selected 'form' element has an empty or non-existent 'action' attribute.
-      /// </exception>
-      /// <exception cref="ArgumentNullException">
-      /// 	<paramref name="formElementsSelection"/> is null.
-      /// </exception>
-      /// <exception cref="ArgumentException">
-      /// 	<paramref name="formElementsSelection"/> is empty.
-      /// </exception>
-      /// <example>
-      /// 	<code>
-      /// selection.Submit(new Preselection {
-      ///   { "input[name='product']", input =&gt; input.Value("NewProduct") },
-      ///   { "input[name='type']", input =&gt; input.Value("NewType") },
-      /// });
-      /// </code>
-      /// </example>
-      public AranaEngine Submit(Preselection formElementsSelection)
-      {
-         if (formElementsSelection == null)
-            throw new ArgumentNullException("formElementsSelection");
-
-         if (formElementsSelection.Count == 0)
-            throw new ArgumentException("Preselection can't be empty.",
-                                        "formElementsSelection");
-
-         return Submit(true, formElementsSelection.Invoke(this.engine));
-      }
-
-
-      /// <summary>
-      /// Submits the selected 'form' element, given its 'action' attribute.
-      /// </summary>
-      /// <returns>An updated <see cref="AranaEngine"/>.</returns>
-      /// <exception cref="InvalidOperationException">
-      /// 1. If the currently selected elements doesn't contain a 'form' element.
-      /// 2. If a currently selected 'form' element doesn't have any attributes.
-      /// 3. If a currently selected 'form' element has an empty or non-existent 'action' attribute.
-      /// </exception>
-      public AranaEngine Submit()
-      {
-         return Submit(true, null);
-      }
-
-
-      /// <summary>
-      /// Submits the selected 'form' element, given its 'action' attribute.
-      /// </summary>
-      /// <param name="requestValues">The request values.</param>
-      /// <returns>An updated <see cref="AranaEngine"/>.</returns>
-      /// <exception cref="InvalidOperationException">
-      /// 1. If the currently selected elements doesn't contain a 'form' element.
-      /// 2. If a currently selected 'form' element doesn't have any attributes.
-      /// 3. If a currently selected 'form' element has an empty or non-existent 'action' attribute.
-      /// </exception>
-      public AranaEngine Submit(NameValueCollection requestValues)
-      {
-         return Submit(true, requestValues);
-      }
-
-      /// <summary>
-      /// Checks a selected radio button or checkbox to be submitted with
-      /// <see cref="Submit()" />.
-      /// </summary>
-      /// <returns>The value of the radio button or checkbox' 'value' attribute.</returns>
-      public Selection Check()
-      {
-         Selection input = Select("input");
-         string value = input.Value();
-
-         if (String.IsNullOrEmpty(value))
-            throw new InvalidOperationException(
-               String.Format("The input field '{0}' does not have a valid 'value'.",
-                             input));
-
-         return input.Value(value);
-      }
-
-      /// <summary>
-      /// Submits the selected 'form' element, given its 'action' attribute.
-      /// </summary>
-      /// <param name="followRedirect"><c>true</c> if the request should automatically follow redirection responses from the
-      /// Internet resource; otherwise, <c>false</c>. The default value is true.</param>
-      /// <param name="requestValues">The request values.</param>
-      /// <returns>An updated <see cref="AranaEngine"/>.</returns>
-      /// <exception cref="InvalidOperationException">
-      /// 	<list type="number">
-      /// 		<item>If the currently selected elements doesn't contain a 'form' element.</item>
-      /// 		<item>If a currently selected 'form' element has an empty or non-existent 'action' attribute and an action URI can't be deduced from a previous <see cref="AranaRequest"/>.</item>
-      /// 	</list>
-      /// </exception>
-      public AranaEngine Submit(bool followRedirect, NameValueCollection requestValues)
-      {
-         Selection form = Select("form");
-
-         string method = form.Attribute("method");
-         // Set the URI to post the form to. Default to the current URI.
-         string action = form.Attribute("action").NullWhenEmpty() ??
-                         this.engine.Uri.ToString();
-
-         if (String.IsNullOrEmpty(action))
-            throw new InvalidOperationException("No valid 'action' to perform.");
-
-         RequestDictionary requestDictionary = null;
-
-         // Set the HTTP method. Default to "GET".
-         method = String.IsNullOrEmpty(method)
-                     ? HttpMethod.Get
-                     : method.ToUpperInvariant();
-
-
-         // TODO: Use GetFormElementsCssSelector() when Fizzler supports it.
-         Selection formElements = this.engine.Select(FormElementsSelector);
-
-         if (formElements.Count > 0)
-            requestDictionary = formElements.GetRequestCollection(requestValues);
-
-         this.engine.NavigateTo(action, followRedirect, method, requestDictionary);
-
-         return this.engine;
       }
 
 
