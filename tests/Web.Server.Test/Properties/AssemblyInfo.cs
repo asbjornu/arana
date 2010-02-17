7f8a1001797afe114d4eae0e18f03324c7192387
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2010-02-17 15:45:34 +0000

    Folder restructuring

---
+++ tests/Web.Server.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Araña WebServer Test Library")]
+[assembly: AssemblyProduct("Araña WebServer Test Library")]
+[assembly: AssemblyDescription("Araña's WebServer unit testing library")]
+[assembly: Guid("71e1d84c-b6ec-48d4-8eb8-dfb86cc1363f")]
